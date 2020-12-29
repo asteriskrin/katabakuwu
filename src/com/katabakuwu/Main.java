7dd7f7cf1d91762a33bbaad49725b2c06bd32a9d
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2020-12-29 09:31:54 +0700

    Create FrameMainMenu class and show FrameMainMenu class in main method

--- src/com/katabakuwu/Main.java
+++ src/com/katabakuwu/Main.java
@@ -1,10 +1,22 @@
 package com.katabakuwu;
 
+import java.awt.EventQueue;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Game is started.");
-
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameMainMenu frame = new FrameMainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
