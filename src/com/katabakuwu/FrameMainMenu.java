a094bb184127fd56e070dfe1475a12769318d20a
Farhan <70105993+farfnd@users.noreply.github.com>
2021-01-01 00:18:15 +0800

    main menu update

--- src/com/katabakuwu/FrameMainMenu.java
+++ src/com/katabakuwu/FrameMainMenu.java
@@ -1,34 +1,61 @@
 package com.katabakuwu;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JButton;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
 
 public class FrameMainMenu extends JFrame {
 
+	private static final long serialVersionUID = 2883848038660074213L;
 	private JPanel contentPane;
 
 	/**
 	 * Create the frame.
+	 * 
 	 */
 	public FrameMainMenu() {
+		super("Katabakuwu");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("assets/logo.jpg"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 412, 521);
+		setBounds(100, 100, 480, 640);
+		
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+
+		JLabel picLabel = new JLabel();
+		picLabel.setLocation(165, 100);
+		picLabel.setSize(150, 150);
+		picLabel.setIcon(new ImageIcon(new ImageIcon("assets/logo.jpg").getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH)));
+		contentPane.add(picLabel);
+		
 		JLabel lblNewLabel = new JLabel("Katabakuwu");
-		lblNewLabel.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 26));
-		lblNewLabel.setBounds(125, 22, 147, 36);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 32));
+		lblNewLabel.setBounds(10, 35, 456, 36);
 		contentPane.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Play");
-		btnNewButton.setBounds(10, 306, 376, 42);
+		JButton btnNewButton = new JButton("Start Game");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.setBounds(10, 306, 446, 60);
 		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Scoreboard");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1.setBounds(10, 386, 446, 60);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("Settings");
+		btnNewButton_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton_1_1.setBounds(10, 466, 446, 60);
+		contentPane.add(btnNewButton_1_1);
 	}
 }
