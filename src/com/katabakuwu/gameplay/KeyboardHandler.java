8323c9fa224edf2ff21173b391d942f3e8b2513f
Farhan <70105993+farfnd@users.noreply.github.com>
2021-01-05 03:18:43 +0800

    Update decrease HP on wrong guess

--- src/com/katabakuwu/gameplay/KeyboardHandler.java
+++ src/com/katabakuwu/gameplay/KeyboardHandler.java
@@ -3,15 +3,17 @@ package com.katabakuwu.gameplay;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
 import javax.swing.JTextField;
 
 import com.katabakuwu.controller.Game;
 
 /**
  * Keyboard Handler.
- * It handles player input for guessing word.
+ * It handles player input through keyboard for guessing word.
  * 
  * @author Ryan Garnet Andrianto
+ * @author Farhan Arifandi
  */
 public class KeyboardHandler implements KeyListener {
 	private Game game;
@@ -33,16 +35,21 @@ public class KeyboardHandler implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		char letter = Character.toUpperCase(e.getKeyChar());
-		
+		// JButton source = (JButton) e.getSource();
 		boolean response = game.getUser().getPlayer().getGuessWord().guess(letter);
 		
 		if(response) {
 			// Guess successful
 			game.getUser().getPlayer().getGuessWord().updateWordDisplay(guessText);
+			// source.setEnabled(false);
 		}
 		else {
 			// Guess failed
 			System.out.println("Guess Failed");
+			game.getUser().getPlayer().getHealth().reduceValue(
+				((float)Math.min(30, game.getUser().getPlayer().getTimer().getDuration())/game.getUser().getPlayer().getTimer().getMaxDuration())*25
+			);
+			game.getUser().getPlayer().getHealth().updateProgressBar();
 		}
 	}
 
