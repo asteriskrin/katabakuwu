fa7a2dd5a3abbd5ab7071a177707e4167202e68e
Farhan <70105993+farfnd@users.noreply.github.com>
2021-01-07 22:39:52 +0800

    update disable empty name input

--- src/com/katabakuwu/server/WordDatabase.java
+++ src/com/katabakuwu/server/WordDatabase.java
@@ -3,6 +3,8 @@ package com.katabakuwu.server;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.katabakuwu.gameplay.Word;
+
 /**
  * Word Database.
  * This class stores words.
@@ -10,16 +12,20 @@ import java.util.Random;
  * @author Ryan Garnet Andrianto
  */
 public class WordDatabase {
-	private ArrayList<String> words;
+	private ArrayList<Word> words;
 	
 	/**
 	 * Constructor
 	 */
 	public WordDatabase() {
-		words = new ArrayList<String>();
-		words.add("APEL");
-		words.add("JERUK");
-		words.add("PISANG");
+		words = new ArrayList<Word>();
+		words.add(new Word("APEL"));
+		words.add(new Word("JERUK"));
+		words.add(new Word("PISANG"));
+		words.add(new Word("SEMANGKA"));
+		words.add(new Word("PEPAYA"));
+		words.add(new Word("JAMBU"));
+		words.add(new Word("MANGGA"));
 	}
 	
 	/**
@@ -27,11 +33,14 @@ public class WordDatabase {
 	 * 
 	 * @return Word as string
 	 */
-	public String getRandomWord() {
+	public Word getRandomWord() {
 		Random rand = new Random();
 		
-		int pos = rand.nextInt(words.size());
-		
-		return words.get(pos);
+		while(true) {
+			int pos = rand.nextInt(words.size());
+			if(!words.get(pos).getStatus()) {
+				return words.get(pos);
+			}
+		}
 	}
 }
